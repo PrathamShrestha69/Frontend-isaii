585f684080aac3989f5f99a0a976e92de7fe87b0
Pratham943 <phoneixwasfound69@gmail.com>
2025-11-05 12:15:35 +0545

    meow

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://isaii-backend.vercel.app/"|| "http://localhost:5000/api",
+  baseURL: "https://isaii-backend.vercel.app/api"|| "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
